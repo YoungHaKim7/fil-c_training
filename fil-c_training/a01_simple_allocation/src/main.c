d395a5159cd31062363507f1fc400b3a7c72ae61
GYoung <ytok1108@aol.com>
2025-11-23 20:58:04 +0900

    fmt

--- fil-c_training/a01_simple_allocation/src/main.c
+++ fil-c_training/a01_simple_allocation/src/main.c
@@ -1,9 +1,8 @@
 #include <stdfil.h>
 #include <stdlib.h>
 
-int main()
-{
-    char* p = malloc(16);
-    zprintf("p = %P\n", p + 42);
-    return 0;
+int main() {
+  char *p = malloc(16);
+  zprintf("p = %P\n", p + 42);
+  return 0;
 }
